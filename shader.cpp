efe906bca7864e5fe714bcbfa93dc66bbb64eab6
julianguarin <julian.guarin.2010@gmail.com>
2019-04-25 19:33:24 -0500

    Day52: Shader System Revamped, try them on a more humble example

--- shader.cpp
+++ shader.cpp
@@ -12,19 +12,20 @@ HDC::ShaderProgram::ShaderProgram(const char* vertexshaderpath, const char* frag
 
     m_vtx_shader = std::make_shared<ShaderSource>(vertexshaderpath, HDC::ShaderType::VTX);
     m_frg_shader = std::make_shared<ShaderSource>(fragmentshaderpath, HDC::ShaderType::FRG);
-
+    m_program_shader = std::make_shared<QOpenGLShaderProgram>();
     if (!m_vtx_shader && !m_frg_shader) {
         Invalidate();
         return;
     }
 
-    auto program = (*this)();
-    program->addShader(m_vtx_shader.get()[0]());
-    program->addShader(m_frg_shader.get()[0]());
+    m_program_shader->addShader(m_vtx_shader.get()[0]());
+    m_program_shader->addShader(m_frg_shader.get()[0]());
+
 
 }
 
 HDC::ShaderProgram::~ShaderProgram(){
+
 
 
 }
