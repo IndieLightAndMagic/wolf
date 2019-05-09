9d1b62f91c83970095b9ddceda9ab377f2e202dc
julianguarin <julian.guarin.2010@gmail.com>
2019-05-09 16:56:07 -0500

    gh: texture enhanced

--- src/texture/texturemanager.cpp
+++ src/texture/texturemanager.cpp
@@ -25,6 +25,10 @@ void HDC::TextureManager::printtextureinformation(unsigned int gltxture){
     assert(found); 
     auto pqimage     = gltxture_imageptr_map[gltxture];
     QImage& rqimage  = *(reinterpret_cast<QImage*>(pqimage));
+
+    auto imageformat = rqimage.format();
+    std::cout << "Image Format is: " << TextureManager::format_map[imageformat] << std::endl;
+
     auto colorformat = rqimage.pixelFormat().typeInterpretation();
 
     if (colorformat  == QPixelFormat::UnsignedInteger ) std::cout << "Color is QPixelFormat::UnsignedInteger " << std::endl;     
@@ -81,37 +85,65 @@ std::deque<unsigned int> HDC::TextureManager::emptytextureslots {
     GL_TEXTURE29,
     GL_TEXTURE30,
     GL_TEXTURE31,
-  
+
 };        
 
-std::pair<unsigned int, bool> HDC::TextureManager::registerimg(std::string imgpath){
-    
+void HDC::TextureManager::imgisnotregistered(std::string imgpath){
+
     assert(HDC::TextureManager::filename_gltxture_map.find(imgpath) == HDC::TextureManager::filename_gltxture_map.end());
+
+}
+QImage* HDC::TextureManager::createimg(std::string imgpath){
+
+    auto pqimage = new QImage(initializeTexture(imgpath));
+    assert(pqimage->isNull() == false);
+    return pqimage;    
+
+}
+
+QImage* HDC::TextureManager::createimg(unsigned int width, unsigned int height, QImage::Format format){
+
+    auto pqimage = new QImage(static_cast<int>(width), static_cast<int>(height), format);
+    assert(pqimage->isNull() == false);
+    return pqimage;    
+
+}
+
+void HDC::TextureManager::generateandregistertxture(std::string imgname, QImage* pqimage){
 
     unsigned int gltxture{0};
     glGenTextures(1, &gltxture);
     assert(HDC::TextureManager::gltxture_txtureslot_map.find(gltxture) == HDC::TextureManager::gltxture_txtureslot_map.end());
-
-    auto pqimage = new QImage(initializeTexture(imgpath));
-    assert(pqimage->isNull() == false);
     
-    filename_gltxture_map[imgpath] = gltxture;
+    filename_gltxture_map[imgname] = gltxture;
     gltxture_imageptr_map[gltxture] = pqimage;
-
-    return std::make_pair(gltxture, configureTexture(gltxture));
 
 }
 
+std::pair<unsigned int, bool> HDC::TextureManager::registerimg(std::string imgpath){
+
+    /*check*/   imgisnotregistered(imgpath);
+    /*then*/    generateandregistertxture(imgpath, createimg(imgpath));
+    /*finally*/ return std::make_pair(filename_gltxture_map[imgpath], configureTexture(filename_gltxture_map[imgpath]));
+
+}
+std::pair<unsigned int, bool> HDC::TextureManager::registerimg(std::string imgname, unsigned int width, unsigned int height, QImage::Format format){
+
+
+    /*check*/   imgisnotregistered(imgname);
+    /*then*/    generateandregistertxture(imgname, createimg(width, height, format));
+    /*finally*/ return std::make_pair(filename_gltxture_map[imgname], configureTexture(filename_gltxture_map[imgname]));
+
+}
 std::pair<std::vector<unsigned int>, bool> HDC::TextureManager::registerimg(std::vector<std::string> imgpaths){
-    
+
     auto gltxtures = std::vector<unsigned int>{};
     for (auto& imgpath : imgpaths){
 
         auto [gltxture, ok] = registerimg(imgpath);
         assert(ok);
-
         gltxtures.push_back(gltxture);
-    
+
     }
     return std::make_pair(gltxtures, true);
 }
@@ -168,7 +200,7 @@ std::tuple<unsigned int, bool> HDC::TextureManager::getslot(std::string imgpath)
 }
 
 bool HDC::TextureManager::unstage(unsigned int gltxture){
-    
+
     if(gltxture_txtureslot_map.find(gltxture) == gltxture_txtureslot_map.end()) return false;
     auto glslot = gltxture_txtureslot_map[gltxture];
     gltxture_txtureslot_map.erase(gltxture);
@@ -177,7 +209,7 @@ bool HDC::TextureManager::unstage(unsigned int gltxture){
 
 }
 bool HDC::TextureManager::unstage(std::string imgpath){
-    
+
     if(filename_gltxture_map.find(imgpath) == filename_gltxture_map.end()) return false;
     auto gltxture = filename_gltxture_map[imgpath];
     if(gltxture_txtureslot_map.find(gltxture) == gltxture_txtureslot_map.end()) return false;
@@ -195,4 +227,35 @@ QImage HDC::TextureManager::initializeTexture(const std::string filename_texture
     return qimage;
 
 }
+
+std::map<QImage::Format, std::string> HDC::TextureManager::format_map{
+    std::make_pair(QImage::Format_Invalid,  "QImage::Format_Invalid:\tThe image is invalid."),
+    std::make_pair(QImage::Format_Mono, "QImage::Format_Mono:\tThe image is stored using 1-bit per pixel. Bytes are packed with the most significant bit (MSB) first."),
+    std::make_pair(QImage::Format_MonoLSB,  "QImage::Format_MonoLSB:\tThe image is stored using 1-bit per pixel. Bytes are packed with the less significant bit (LSB) first."),
+    std::make_pair(QImage::Format_Indexed8, "QImage::Format_Indexed8:\tThe image is stored using 8-bit indexes into a colormap."),
+    std::make_pair(QImage::Format_RGB32,    "QImage::Format_RGB32:\tThe image is stored using a 32-bit RGB format (0xffRRGGBB)."),
+    std::make_pair(QImage::Format_ARGB32,   "QImage::Format_ARGB32:\tThe image is stored using a 32-bit ARGB format (0xAARRGGBB)."),
+    std::make_pair(QImage::Format_ARGB32_Premultiplied, "QImage::Format_ARGB32_Premultiplied:\tThe image is stored using a premultiplied 32-bit ARGB format (0xAARRGGBB), i.e. the red, green, and blue channels are multiplied by the alpha component divided by 255. (If RR, GG, or BB has a higher value than the alpha channel, the results are undefined.) Certain operations (such as image composition using alpha blending) are faster using premultiplied ARGB32 than with plain ARGB32."),
+    std::make_pair(QImage::Format_RGB16,    "QImage::Format_RGB16:\tThe image is stored using a 16-bit RGB format (5-6-5)."),
+    std::make_pair(QImage::Format_ARGB8565_Premultiplied,   "QImage::Format_ARGB8565_Premultiplied:\tThe image is stored using a premultiplied 24-bit ARGB format (8-5-6-5)."),
+    std::make_pair(QImage::Format_RGB666,   "QImage::Format_RGB666:\tThe image is stored using a 24-bit RGB format (6-6-6). The unused most significant bits is always zero."),
+    std::make_pair(QImage::Format_ARGB6666_Premultiplied,   "QImage::Format_ARGB6666_Premultiplied:\tThe image is stored using a premultiplied 24-bit ARGB format (6-6-6-6)."),
+    std::make_pair(QImage::Format_RGB555,   "QImage::Format_RGB555:\tThe image is stored using a 16-bit RGB format (5-5-5). The unused most significant bit is always zero."),
+    std::make_pair(QImage::Format_ARGB8555_Premultiplied,   "QImage::Format_ARGB8555_Premultiplied:\tThe image is stored using a premultiplied 24-bit ARGB format (8-5-5-5)."),
+    std::make_pair(QImage::Format_RGB888,   "QImage::Format_RGB888:\tThe image is stored using a 24-bit RGB format (8-8-8)."),
+    std::make_pair(QImage::Format_RGB444,   "QImage::Format_RGB444:\tThe image is stored using a 16-bit RGB format (4-4-4). The unused bits are always zero."),
+    std::make_pair(QImage::Format_ARGB4444_Premultiplied,   "QImage::Format_ARGB4444_Premultiplied:\tThe image is stored using a premultiplied 16-bit ARGB format (4-4-4-4)."),
+    std::make_pair(QImage::Format_RGBX8888, "QImage::Format_RGBX8888:\tThe image is stored using a 32-bit byte-ordered RGB(x) format (8-8-8-8). This is the same as the Format_RGBA8888 except alpha must always be 255. (added in Qt 5.2)"),
+    std::make_pair(QImage::Format_RGBA8888, "QImage::Format_RGBA8888:\tThe image is stored using a 32-bit byte-ordered RGBA format (8-8-8-8). Unlike ARGB32 this is a byte-ordered format, which means the 32bit encoding differs between big endian and little endian architectures, being respectively (0xRRGGBBAA) and (0xAABBGGRR). The order of the colors is the same on any architecture if read as bytes 0xRR,0xGG,0xBB,0xAA. (added in Qt 5.2)"),
+    std::make_pair(QImage::Format_RGBA8888_Premultiplied,   "QImage::Format_RGBA8888_Premultiplied:\tThe image is stored using a premultiplied 32-bit byte-ordered RGBA format (8-8-8-8). (added in Qt 5.2)"),
+    std::make_pair(QImage::Format_BGR30,    "QImage::Format_BGR30:\tThe image is stored using a 32-bit BGR format (x-10-10-10). (added in Qt 5.4)"),
+    std::make_pair(QImage::Format_A2BGR30_Premultiplied,    "QImage::Format_A2BGR30_Premultiplied:\tThe image is stored using a 32-bit premultiplied ABGR format (2-10-10-10). (added in Qt 5.4)"),
+    std::make_pair(QImage::Format_RGB30,    "QImage::Format_RGB30:\tThe image is stored using a 32-bit RGB format (x-10-10-10). (added in Qt 5.4)"),
+    std::make_pair(QImage::Format_A2RGB30_Premultiplied,    "QImage::Format_A2RGB30_Premultiplied:\tThe image is stored using a 32-bit premultiplied ARGB format (2-10-10-10). (added in Qt 5.4)"),
+    std::make_pair(QImage::Format_Alpha8,   "QImage::Format_Alpha8:\tThe image is stored using an 8-bit alpha only format. (added in Qt 5.5)"),
+    std::make_pair(QImage::Format_Grayscale8,   "QImage::Format_Grayscale8:\tThe image is stored using an 8-bit grayscale format. (added in Qt 5.5)"),
+    std::make_pair(QImage::Format_RGBX64,   "QImage::Format_RGBX64:\tThe image is stored using a 64-bit halfword-ordered RGB(x) format (16-16-16-16). This is the same as the Format_RGBX64 except alpha must always be 65535. (added in Qt 5.12)"),
+    std::make_pair(QImage::Format_RGBA64,   "QImage::Format_RGBA64:\tThe image is stored using a 64-bit halfword-ordered RGBA format (16-16-16-16). (added in Qt 5.12)"),
+    std::make_pair(QImage::Format_RGBA64_Premultiplied, "QImage::Format_RGBA64_Premultiplied:\tThe image is stored using a premultiplied 64-bit halfword-ordered RGBA format (16-16-16-16). (added in Qt 5.12)"),
+}; 
 
--- src/texture/texturemanager.h
+++ src/texture/texturemanager.h
@@ -15,22 +15,23 @@
 #include <QOpenGLShaderProgram>
 
 namespace HDC {
-    class TextureManager {
+	class TextureManager {
 
+        static std::map<QImage::Format, std::string>    format_map;
+        static std::map<std::string, unsigned int>      filename_gltxture_map;
+        static std::map<unsigned int, unsigned int>     gltxture_txtureslot_map;
+        static std::map<unsigned int, void*>            gltxture_imageptr_map;
 
-        static std::map<std::string, unsigned int>  filename_gltxture_map;
-        static std::map<unsigned int, unsigned int> gltxture_txtureslot_map;
-        static std::map<unsigned int, void*>        gltxture_imageptr_map;
+        static std::map<std::string, unsigned int>      filename_index_map;
+        static std::map<unsigned int, void*>            index_ptrimage_map; 
 
-        static std::map<std::string, unsigned int>  filename_index_map;
-        static std::map<unsigned int, void*>        index_ptrimage_map; 
+        static std::vector<unsigned int>                stagedtextures;
+        static std::deque<unsigned int>                 emptytextureslots;
 
-        static std::vector<unsigned int>            stagedtextures;
-        static std::deque<unsigned int>             emptytextureslots;
-    
     public:
         static std::pair<std::vector<unsigned int>, bool>   registerimg(std::vector<std::string>);
         static std::pair<unsigned int, bool>                registerimg(std::string);
+        static std::pair<unsigned int, bool> 				registerimg(std::string, unsigned int, unsigned int, QImage::Format);
         static std::tuple<unsigned int, bool>               getslot(std::string);
         static std::tuple<unsigned int, bool>               getslot(unsigned int);
         static bool                                         unstage(std::string);
@@ -38,9 +39,16 @@ namespace HDC {
         static void*                                        getimageptr(unsigned int);
         static void                                         printtextureinformation(unsigned int); 
     private:
+
         static constexpr unsigned int                       slot(unsigned int uislot);        
         static QImage                                       initializeTexture(const std::string filename_texture);
         static bool                                         configureTexture(unsigned int);
+
+        static void                               imgisnotregistered(std::string);
+        static QImage*                            createimg(std::string);
+        static QImage*                            createimg(unsigned int, unsigned int, QImage::Format);
+        static void                               generateandregistertxture(std::string, QImage*);
+
         
     };
 
