8e81d22de41c1fe4bcafeea196cfd69582d1633e
julianguarin <julian.guarin.2010@gmail.com>
2019-05-09 12:21:06 -0500

    heathmap sample

---
+++ samples/heathmap/hmap.cpp
@@ -0,0 +1,192 @@
+#include "courtdemo.h"
+
+#include <tuple>
+#include <memory>
+#include <string>
+#include <cassert>
+#include <iostream>
+
+#include <QDir>
+#include <QScreen>
+#include <QMatrix4x4>
+#include <QElapsedTimer>
+#include <QGuiApplication>
+#include <QOpenGLShaderProgram>
+
+
+
+
+HDC::IntroScene::IntroScene(){
+    
+
+}
+
+
+
+void HDC::IntroScene::initializeGeometry(){
+
+    m_soccer_court = new HDC::TexturedPlaneGeometry();
+
+    m_soccer_court->setheight(0.3f);
+    m_soccer_court->setwidth(0.3f);
+    m_soccer_court->resetplane();
+
+
+}
+void HDC::IntroScene::processState(){
+
+    float sliderValue{0.0f};
+
+    m_elapsedTimeMeasurement = m_timer.elapsed();
+
+    auto m_program = shaderProgram.get()[0]();    
+    if (m_state == IntroState::START){
+
+        m_state = IntroState::FADING_IN_CARNAGE;
+        std::cout << "CARNAGE\n";
+        m_timer.start();
+        m_timeToExpire = 1000.0f;
+
+        //As texture is going to be the same always, lets attach it.
+        m_program->setUniformValue(m_textureUniform, 0);
+
+    } else if (m_state == IntroState::FADING_IN_CARNAGE){
+
+        float _timeFraction = m_elapsedTimeMeasurement / m_timeToExpire;
+        _timeFraction = _timeFraction <= 1.0f ? _timeFraction : 1.0f;
+
+
+        if (_timeFraction >= 1.0f) {
+            std::cout << "STILL IN CARNAGE\n";
+            m_state = IntroState::STILL_IN_CARNAGE;
+            m_timer.start();
+            m_timeToExpire = 3000.0f;
+        }
+        sliderValue = _timeFraction;
+
+    } else if (m_state == IntroState::STILL_IN_CARNAGE){
+
+        float _timeFraction = m_elapsedTimeMeasurement / m_timeToExpire;
+        _timeFraction = _timeFraction < 1.0f ? _timeFraction : 1.0f;
+
+        if (_timeFraction >= 1.0f) {
+            std::cout << "FADING_OUT_CARNAGE\n";
+            m_state = IntroState::FADING_OUT_CARNAGE;
+            m_timer.start();
+            m_timeToExpire = 1000.0f;
+        }
+        sliderValue = 1.0f;
+
+
+    } else if (m_state == IntroState::FADING_OUT_CARNAGE){
+
+        float _timeFraction = m_elapsedTimeMeasurement / m_timeToExpire;
+        _timeFraction = 1.0 - _timeFraction;
+        _timeFraction = _timeFraction >= 0.0f ? _timeFraction : 0.0f;
+
+        if (_timeFraction <= 0.0f) {
+            std::cout << "FADING IN CLASSIC\n";
+
+            m_state = IntroState::FADING_CLASSIC_IN;
+            m_timer.start();
+            m_timeToExpire = 1000.0f;
+        }
+        sliderValue = _timeFraction;
+
+    } else if (m_state == IntroState::FADING_CLASSIC_IN){
+
+        float _timeFraction = m_elapsedTimeMeasurement / m_timeToExpire;
+        _timeFraction = _timeFraction <= 1.0f ? _timeFraction : 1.0f;
+
+        if (_timeFraction >= 1.0f) {
+            m_state = IntroState::IDLE;
+            m_timer.start();
+            m_timeToExpire = 3000.0f;
+        }
+        sliderValue = _timeFraction;
+
+        //As texture is going to be the same always, lets attach it.
+        m_program->setUniformValue(m_textureUniform, m_soccer_court_texture);
+
+    } else if(m_state == IntroState::FINISH){
+
+        QGuiApplication::exit();
+
+    } else {
+
+        sliderValue = 1.0f;
+
+    }
+    m_program->setUniformValue(m_pcsSliderUniform, sliderValue);    
+
+}
+void HDC::IntroScene::initialize(){
+
+
+    std::cout <<"Wolfy resources:" << std::endl;
+
+    initializeGeometry();
+    shaderProgram = std::make_shared<ShaderProgram>(
+        (std::string(RESOURCES_DIR) + "/shaders/wolfy_intro.vert").c_str(),    
+        (std::string(RESOURCES_DIR) + "/shaders/wolfy_intro.frag").c_str()
+        );
+    fastShaderProgram = shaderProgram.get()[0]();
+
+    //Register Textures
+    //Stage Textures
+    auto [gltxture, bok] = HDC::TextureManager::registerimg(std::string{RESOURCES_DIR} + "/textures/soccerfieldgrass.png");
+    assert(bok);
+    auto slot_bok = HDC::TextureManager::getslot(gltxture);
+    assert(std::get<1>(slot_bok));
+    m_soccer_court_texture = std::get<0>(slot_bok);
+    TextureManager::printtextureinformation(gltxture);
+
+
+    auto m_program = shaderProgram.get()[0]();    
+
+    m_posAttr = m_program->attributeLocation("posAttr");
+    m_colAttr = m_program->attributeLocation("colAttr");
+    m_texAttr = m_program->attributeLocation("texAttr");
+    m_matrixUniform = m_program->uniformLocation("matrix");
+    m_textureUniform = m_program->uniformLocation("ourTexture");
+    m_pcsSliderUniform = m_program->uniformLocation("pcsSlider");
+
+
+    m_valid = true;
+    std::cout << "\n\tShaders [OK]" << std::endl; 
+
+
+    m_cam.setCamera();
+
+
+    installEventFilter(&m_im);
+    connect(&m_im, &InputManager::escape,
+        this, &HDC::IntroScene::handleEscape);
+    connect(&m_im, &InputManager::up_arrow,
+        this, &HDC::IntroScene::handleUp);
+    connect(&m_im, &InputManager::down_arrow,
+        this, &HDC::IntroScene::handleDown);
+
+}
+
+void HDC::IntroScene::render(){
+
+    Scene::render();
+    fastShaderProgram->bind();
+    m_soccer_court->enable();
+    fastShaderProgram->setUniformValue(m_matrixUniform, m_cam.getCamera());
+    processState();
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    fastShaderProgram->release();
+
+}
+void HDC::IntroScene::handleEscape(){
+    std::cout << "Finishing Scene....." << std::endl;
+    m_state = IntroState::FINISH;
+}
+void HDC::IntroScene::handleUp(){
+    m_cam.setCameraPositionDelta(0.0f, 0.0f, 0.1f);
+}
+void HDC::IntroScene::handleDown(){
+    m_cam.setCameraPositionDelta(0.0f, 0.0f, -0.1f);
+}
---
+++ samples/heathmap/hmap.h
@@ -0,0 +1,76 @@
+#ifndef _INTROSCENE_H_
+#define _INTROSCENE_H_
+
+#include "scene.h"
+#include "camera.h"
+#include "shader.h"
+#include "geometry.h"
+#include "texturemanager.h"
+
+
+#include <memory>
+#include <QImage>
+#include <QtCore/QElapsedTimer>
+#include <QtGui/QOpenGLShaderProgram>
+#include <QtGui/QOpenGLFunctions>
+#include <QtGui/QOpenGLFunctions_4_3_Core>
+
+
+namespace HDC{
+    class IntroScene : public Scene
+    {
+    public:
+        Camera m_cam;
+        enum class IntroState {
+            START, 
+            FADING_IN_CARNAGE, 
+            STILL_IN_CARNAGE, 
+            FADING_OUT_CARNAGE, 
+            FADING_CLASSIC_IN, 
+            STILL_IN_CLASSIC, 
+            FADING_OUT_CLASSIC,
+            IDLE,
+            FINISH
+        };
+
+        QImage qimage{};
+        std::vector<QImage> qimages{};
+        
+        IntroScene();
+
+        void initialize() override;
+        void render() override;
+
+    private:
+
+        void processState();
+        void initializeShaders(QOpenGLShader::ShaderType type, const char* path);
+        void bakeShader();
+        void initializeGeometry();
+        void handleEscape();
+        void handleUp();
+        void handleDown();
+
+        GLuint m_posAttr;
+        GLuint m_colAttr;
+        GLuint m_texAttr;
+        GLuint m_matrixUniform;
+        GLuint m_textureUniform;
+        GLuint m_pcsSliderUniform;
+        
+        GLuint m_soccer_court_texture;
+        HDC::TexturedPlaneGeometry* m_soccer_court{nullptr};
+
+        std::shared_ptr<HDC::ShaderProgram> shaderProgram;
+        QOpenGLShaderProgram* fastShaderProgram;
+
+        QElapsedTimer m_timer;
+        uint64_t m_elapsedTimeMeasurement{0};
+
+        IntroScene::IntroState m_state{IntroState::START};
+        float m_timeToExpire{0.0f};
+        unsigned int m_activeTextureIndex{0};
+
+    };
+}
+#endif /* _INTROSCENE_H_ */
