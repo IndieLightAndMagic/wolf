e643aa6dec37f208e250f5bbceeedab7b112f367
julianguarin <julian.guarin.2010@gmail.com>
2019-05-14 15:51:05 -0500

    Added files for FrameBufferObject

---
+++ src/opengl/fbo/FrameBufferObject.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Guarin on 2019-05-14.
+//
+
+#include "FrameBufferObject.h"
---
+++ src/opengl/fbo/FrameBufferObject.h
@@ -0,0 +1,14 @@
+//
+// Created by Guarin on 2019-05-14.
+//
+
+#ifndef COURTDEMO_FRAMEBUFFEROBJECT_H
+#define COURTDEMO_FRAMEBUFFEROBJECT_H
+
+
+class FrameBufferObject {
+
+};
+
+
+#endif //COURTDEMO_FRAMEBUFFEROBJECT_H
